5b6ae5975e2c1e8d2f448fdd797fb8d0b14c1a4a
jconklin329 <103670800+jconklin329@users.noreply.github.com>
2022-09-23 09:40:57 -0400

    I changed the constructor for player because it seemed difficult to set the player_ip address properly with how we have things structured. Instead there's a setter for IP that the BattleshipGame class will call instead.
    I also tried to incorporate some method for the Tile class to keep track of it has an overlap or not but I'm not sure if it's effective or not.

--- Model/Game.java
+++ Model/Game.java
@@ -5,7 +5,19 @@ public class BattleshipGame
 
 	public void startGame(string player1_ip, string player2_ip)
 	{
+		// TODO: get player_ip from client/server interaction
+		player_list[0].setIP(player1_ip);
+		player_list[1].setIP(player2_ip);
 
+		while (!(player_list[0].hasLost()) && !(player_list[1].hasLost()))
+		{
+			
+
+			// this line of code toggles between 1 and 0 by using XOR operation
+			// 0 ^ 1 = 1
+			// 1 ^ 1 = 0
+			current_turn ^= 1;
+		}
 	}
 
 	public bool checkHit(Player player1, Player player2)
@@ -15,6 +27,7 @@ public class BattleshipGame
 
 	BattleshipGame()
 	{
-
+		current_turn = 0;
+		player_list = new Player[2];
 	}
 }
