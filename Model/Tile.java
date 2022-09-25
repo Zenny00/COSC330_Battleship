809c04f939b8352a3f680d122d90344090c9ef4e
Josh <99524099+Zenny00@users.noreply.github.com>
2022-09-25 08:29:20 -0400

    Added constructors and additional tile functions

--- Model/Tile.java
+++ Model/Tile.java
@@ -5,16 +5,21 @@ public class Tile
 	private TileType type;
 	private bool clickable;
 
-	Tile(){clickable = true;}
+        //Various constructors taking different numbers of parameters 
+	public Tile(){clickable = true; }
+        public Tile(int x, int y) { this.x = x; this.y = y; type = SEA; }
+        public Tile(int x, int y, TileType type) { this,x = x; this.y = y; this.type = type; }
+        public Tile(int x, int y, TileType type, bool clickable) { this.x = x, this.y = y; this.type = type; this.clickable = clickable; }
 
 	public bool isClickable(){return clickable;}
 	public int getX(){return x;}
 	public int getY(){return y;}
-	public TileType getTileType(){return type;}
+	public TileType getTileType(){return type;} 
 
 	private void addHit(){type = HIT;}
 	private void addMiss(){type = MISS;}
 	public void setType(TileType status){type = status;}
+        public void setClickability(bool clickability) { clickable = clickability; }
 
 	public TileType addShot() {
 		if (type == SHIP)
