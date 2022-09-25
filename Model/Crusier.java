a8bdf8478db203c41158c019f351605e75789362
Josh <99524099+Zenny00@users.noreply.github.com>
2022-09-25 08:42:26 -0400

    Added display to Crusier

--- Model/Crusier.java
+++ Model/Crusier.java
@@ -1,7 +1,13 @@
 public class Crusier extends Ship
 {
+        //Creates a Crusier at a given origin
 	public Crusier(Tile origin)
 	{
 		super(3, 0, 0, origin, CRUSIER);	
 	}
+
+        public void display()
+        {
+             //TODO call appropriate functions here
+        }
 }
