f5a9ce9bb96216863d1a01dfd0e37c070ec368af
jcomfort1 <joshuacomfort@gmail.com>
2022-10-05 12:19:48 -0400

    Added comments to the drag and drop

--- DragFrame.java
+++ DragFrame.java
@@ -1,3 +1,7 @@
+//Name: Joshua Comfort
+//Date: 10/3/2022
+//Description: Setup the frame and add the icon panels
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -5,13 +9,16 @@ import javax.swing.border.Border;
 
 public class DragFrame extends JFrame
 {
+	//Constructor
 	DragFrame()
 	{
+		//Configure frame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(800, 600);
 		this.setTitle("Drag and Drop Demo");
 		this.setLocationRelativeTo(null);
 		
+		//Add two panels to the frame
 		Border br = BorderFactory.createLineBorder(Color.black);
 		Container c = getContentPane();
  		//new ImageIcon("./View/Graphics/Ships/Submarine.png");
@@ -22,6 +29,7 @@ public class DragFrame extends JFrame
 		ImagePanel submarine = new ImagePanel("./View/Graphics/Ships/Submarine.png", 100, 300);
 		c.add(submarine);
 
+		//Set visible to true
 		this.setVisible(true);
 	}
 }
