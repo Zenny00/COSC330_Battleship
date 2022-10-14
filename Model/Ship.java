f0d26c3117a3607d3a9942af4ba8955ed7bcf73c
jconklin329 <103670800+jconklin329@users.noreply.github.com>
2022-10-14 10:45:33 -0400

    just testing ideas for ship constructor as alternatives to subclasses although subclasses might be better for this. Do we need getters for the icons?

--- Model/Ship.java
+++ Model/Ship.java
@@ -6,19 +6,30 @@ public class Ship
 	private Tile origin;
 	private	ShipType type;
 	private boolean placed = false;
+	private String[] icons; //Possible idea for handling icons, or we go back to subclasses
+	private String baseIcon;
 
 	Ship(ShipType type)
 	{
+		// TODO: turn into a constant later
+		String base = "../View/Graphics/Ships/ShipMod/";
 		this.type = type;
 		switch (type) {
 			case SUBMARINE: 
 				size = 3;
+				icons = new String[]{base + "SubmarineTile1.png",
+									 base + "SubmarineTile2.png",
+									 base + "SubmarineTile3.png"};
+				baseIcon = "../View/Graphics/Ships/Submarine.png";
 				break;
 			case CRUISER: 
 				size = 3;
 				break;
 			case DESTROYER:
 				size = 2;
+				icons = new String[]{base + "DestroyerTile1.png",
+									 base + "DestroyerTIle2.png"};
+				baseIcon = "../View/Graphics/Ships/Destroyer.png";
 				break;
 			case CARRIER:
 				size = 5;
