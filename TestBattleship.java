a27c48b6513eb76a5f6643ac48b10fd14be5cab8
jcomfort1 <joshuacomfort@gmail.com>
2022-11-28 11:38:00 -0500

    Corrected JUnitCommands documentation

--- TestBattleship.java
+++ TestBattleship.java
@@ -1,12 +1,21 @@
 //JUnit testing for Java Battleship
 import org.junit.Test;
-import org.junit.Assert.fail;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
 public class TestBattleship
 {
+	//Fail case
 	@Test
 	public void testFail()
 	{
 		fail("Tests failed!");
+	}
+
+	//Pass case
+	@Test
+	public void testTrue()
+	{
+		assertTrue(true);
 	}
 }
